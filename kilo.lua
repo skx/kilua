7ecd54b813aadfd9ce7dadcd238772c22d823a8a
Steve Kemp <steve@steve.org.uk>
2016-07-14 10:53:04 +0300

    Use a keymap for keybindings.
    This is cleaner and neater.

--- kilo.lua
+++ kilo.lua
@@ -1,26 +1,62 @@
 
-function on_key(k)
+--
+-- Keymap of bound keys
+--
+local keymap = {}
 
-   --
-   -- Expand values less than "a" to be Ctrl-X.
-   --
+--
+--  Default bindings
+--
+keymap['^A'] = sol
+keymap['^E'] = eol
+keymap['^D'] = function() insert( os.date() ) end
+
+
+
+--
+-- Expand values less than "a" to be Ctrl-X.
+--
+-- TODO: Expand PAGE_UP, ARROW_UP, etc.
+--
+function expand_key(k)
    if ( string.byte(k) < string.byte('a') ) then
       k = "^" .. ( string.char( string.byte(k) + string.byte('A')-1 ))
    end
+   return k
+end
+
+
+--
+-- This function is called when input is received.
+--
+-- It will lookup the value of the keypress, and if it
+-- represents a binding it will execute that function.
+--
+-- If no binding is found then the key is inserted into
+-- the editor, literally.
+--
+function on_key(k)
 
    --
-   -- Handle input
+   -- Expand the character we've received to deal with
+   -- control-codes, & etc.
    --
-   if ( k == "^A" ) then
-      -- Ctrl-A -> Start of line
-      sol()
-   elseif ( k == "^E" ) then
-      -- Ctrl-E -> End of line.
-      eol()
-   else
-      -- Insert the character.
-      insert(k)
-   end
+   k = expand_key(k)
+
+   --
+   -- Lookup the function in the key-map.
+   --
+   local func = keymap[k]
+
+   --
+   -- If found, execute it.
+   --
+   if ( func ~= nil ) then func() return end
+
+   --
+   -- Otherwise just insert the character.
+   --
+   insert(k)
 end
 
 
