ce76def5185f9562b8071f7ae373dc0547994291
Steve Kemp <steve@steve.org.uk>
2016-07-17 05:50:43 +0300

    Ctrl-m == '\n', which I always forget.

--- kilo.lua
+++ kilo.lua
@@ -52,7 +52,8 @@ keymap['^H']        = delete
 keymap['^J']        = function() goto_mark() end
 keymap['^K']        = function() kill_line() end
 keymap['^L']        = eval
-keymap['^M']        = function() record_mark() end
+keymap['^M']        = function() insert("\n" ) end
+keymap['^N']        = function() record_mark() end
 keymap['^Q']        = function() quit() end
 keymap['^O']        = open
 keymap['^R']        = function() cmd = prompt( "execute:" ); if ( cmd ) then insert( cmd_output(cmd) ) end end
