6f6a369dc6d8745861118340b87552e63c562f3c
Steve Kemp <steve@steve.org.uk>
2016-07-29 05:27:52 +0300

    make indent

--- src/main.cc
+++ src/main.cc
@@ -10,24 +10,25 @@ void setup()
     initscr();
     start_color();
 
-    if ( has_colors() )
+    if (has_colors())
     {
-	init_pair(1,  COLOR_RED,     COLOR_BLACK);
-	init_pair(2,  COLOR_GREEN,   COLOR_BLACK);
-	init_pair(3,  COLOR_YELLOW,  COLOR_BLACK);
-	init_pair(4,  COLOR_BLUE,    COLOR_BLACK);
-	init_pair(5,  COLOR_MAGENTA, COLOR_BLACK);
-	init_pair(6,  COLOR_CYAN,    COLOR_BLACK);
-	init_pair(7,  COLOR_BLUE,    COLOR_BLACK);
-	init_pair(8,  COLOR_WHITE,   COLOR_BLACK);
+        init_pair(1,  COLOR_RED,     COLOR_BLACK);
+        init_pair(2,  COLOR_GREEN,   COLOR_BLACK);
+        init_pair(3,  COLOR_YELLOW,  COLOR_BLACK);
+        init_pair(4,  COLOR_BLUE,    COLOR_BLACK);
+        init_pair(5,  COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(6,  COLOR_CYAN,    COLOR_BLACK);
+        init_pair(7,  COLOR_BLUE,    COLOR_BLACK);
+        init_pair(8,  COLOR_WHITE,   COLOR_BLACK);
     }
     else
     {
         endwin();
-        fprintf(stderr, "No colours!\n" );
+        fprintf(stderr, "No colours!\n");
         exit(1);
     }
-        raw();
+
+    raw();
     keypad(stdscr, TRUE);
     noecho();
     timeout(750);
