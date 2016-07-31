fa404090b1f391fa06aed1b9ac5d0805f51971f6
Steve Kemp <steve@steve.org.uk>
2016-07-31 13:17:36 +0300

    Moved functions into the correct group

--- src/lua_primitives.h
+++ src/lua_primitives.h
@@ -58,7 +58,6 @@ extern int sol_lua(lua_State *L);
 extern int delete_lua(lua_State *L);
 extern int dirty_lua(lua_State *L);
 extern int exit_lua(lua_State *L);
-extern int height_lua(lua_State *L);
 extern int insert_lua(lua_State *L);
 extern int key_lua(lua_State *L);
 extern int open_lua(lua_State *L);
@@ -66,12 +65,13 @@ extern int point_lua(lua_State *L);
 extern int prompt_lua(lua_State *L);
 extern int save_lua(lua_State *L);
 extern int search_lua(lua_State *L);
+extern int status_lua(lua_State *L);
 
 /*
  * Screen.
  */
 extern int at_lua(lua_State *L);
-extern int status_lua(lua_State *L);
+extern int height_lua(lua_State *L);
 extern int width_lua(lua_State *L);
 
 /*
@@ -83,8 +83,8 @@ extern int syntax_lua(lua_State *L);
  * Buffers
  */
 extern int buffer_lua(lua_State *L);
-extern int buffers_lua(lua_State *L);
 extern int buffer_name_lua(lua_State *L);
+extern int buffers_lua(lua_State *L);
 extern int choose_buffer_lua(lua_State *L);
 extern int create_buffer_lua(lua_State *L);
 extern int kill_buffer_lua(lua_State *L);
